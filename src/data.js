15331842e5c2151f991e1940a54800a13248d85a
GautamMahto <gautammahtoaiie31aii.ac.in@gmail.com>
2025-04-27 18:29:42 +0530

    changes dones in .data file

--- src/data.js
+++ src/data.js
@@ -72,97 +72,7 @@ export const heroImages = [
         'Energy efficient motor and fan technology',
         'Customization options available'
       ]
-    },
-    {
-      title: 'Industrial Blower',
-      image: 'Industrial_Blower.png',
-      features: [
-        'Optimized airflow capacity',
-        'Engineered to overcome system resistance',
-        'Robust motor power options',
-        'Centrifugal and positive displacement available'
-      ]
-    },
-    {
-      title: 'Screw Conveyor',
-      image: 'screw_conver_1.jpg',
-      features: [
-        'Precise transport capacity',
-        'Durable gearbox design',
-        'Adjustable speed control',
-        'Custom configurations available'
-      ]
-    },
-    {
-      title: 'Tray Dryer',
-      image: 'Tray_dyrer.png',
-      features: [
-        'Efficient drying capacity',
-        'Accurate temperature control',
-        'Energy efficient performance',
-        'Customizable features'
-      ]
-    },
-    {
-      title: 'Temperature Minimizing Fan',
-      image: 'Temp_minizing_fan.jpg',
-      features: [
-        'Effective cooling capacity',
-        'Adjustable speed settings',
-        'Energy efficient design',
-        'Tailored designs available'
-      ]
-    },
-    {
-      title: 'SS Solution Tank',
-      image: 'ss_solution_tank.jpg',
-      features: [
-        'Multiple capacity options',
-        'Durable construction',
-        'Customizable agitation & mixing',
-        'Configurable features'
-      ]
-    },
-    {
-      title: 'Electrical Panel Box',
-      image: 'electrical_panel_box_1.png',
-      features: [
-        'Various size and capacity options',
-        'Durable construction for harsh environments',
-        'Custom configurations available',
-        'Advanced safety features'
-      ]
-    },
-    {
-      title: 'Root Blower Filter',
-      image: 'root_blower_1.png',
-      features: [
-        'High airflow capacity',
-        'Robust and durable design',
-        'Oil-free operation',
-        'Energy efficient performance'
-      ]
-    },
-    {
-      title: 'Non-Return Valve',
-      image: 'nrv.png',
-      features: [
-        'Cast iron durability',
-        'Prevents backflow effectively',
-        'Low maintenance',
-        'High-temperature resistance'
-      ]
-    },
-    {
-      title: 'Dry Bag Suction Filter',
-      image: 'dry_bag_suction.png',
-      features: [
-        'Efficient dust and particle collection',
-        'Durable construction',
-        'Easy maintenance',
-        'Customizable options'
-      ]
-    },
+    }
   ];
   
   export const contactInfo = {
@@ -171,17 +81,5 @@ export const heroImages = [
     address: '135, Raghuvir Industrial Part-2, Nr. Platinum Estate, Opp. Vatva Railway Station, GIDC Vatva, Ahmedabad-382445'
   };
 
-  export const clientsList = [
-    { name: 'Air_Control', logo: 'Logos/Air control & chemical.jpeg' },
-    { name: 'ACME Air', logo: 'Logos/Acme Air equipments.jpg' },
-    { name: 'Prashant', logo: 'Logos/Prashant logo.png' },
-    { name: 'ivc', logo: 'Logos/ivc pumps.jpg' },
-    { name: 'macro', logo: 'Logos/macro_black.png' },
-    { name: 'shree', logo: 'Logos/Shree ambica engineering.png' },
-    { name: 'goair', logo: 'Logos/Go air Engineering.png' },
-    { name: 'limboj', logo: 'Logos/limboj.png'}
-
-
-    // Add more as needed
-  ];
+  
   
